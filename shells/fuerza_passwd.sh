dde6b73f665433a12475d9705314f913bba4fe69
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2013-10-07 17:21:17 -0400

    orden de cambio password

--- shells/fuerza_passwd.sh
+++ shells/fuerza_passwd.sh
@@ -1,13 +1,14 @@
 #!/bin/ksh
 # fuerza_passwd.sh server
-RUTA=`pwd`
-cuenta=`cat $RUTA/user_pass.txt | wc -l`
+# Formato 'archivoPassword': username;password
+archivoPassword='user_pass.txt'
+cuenta=`cat $archivoPassword | wc -l`
 cuenta=$(( cuenta - 0 ))
 while [ $cuenta -ge  1 ]
       do
-      linea=`cat $RUTA/user_pass.txt | tail -$cuenta | head -1`
+      linea=`cat $archivoPassword | tail -$cuenta | head -1`
       usrname=`echo $linea | awk -F";" '{print $1}'`
-          usrpass=`echo $linea | awk -F";" '{print $2}'`
+      usrpass=`echo $linea | awk -F";" '{print $2}'`
 echo "####################   "$1";"$linea"   #####################"
 ssh $1 passwd $usrname << ff
 $usrpass
