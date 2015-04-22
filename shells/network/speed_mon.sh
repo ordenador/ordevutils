6092a765141066876617b3df21eb3f9fb4c1e16f
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2015-04-22 18:26:32 -0300

    Update speed_mon.sh

--- shells/network/speed_mon.sh
+++ shells/network/speed_mon.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 fecha=`date '+%F'`
 hora=`date '+%T'`
-output=$(time dd if=/dev/zero count=10 bs=1024k | ssh nombre-o-ip-servidor dd of=/dev/null 2>&1 > /d                                                                                                   ev/null | awk '$0' ORS='\n')
+output=$(time dd if=/dev/zero count=10 bs=1024k | ssh nombre-o-ip-servidor dd of=/dev/null 2>&1 > /dev/null | awk '$0' ORS='\n')
 bytes=`echo -e "${output}" | grep copied | awk '{print $1}'`  ## obtener bytes
 kilobytes=$(( $bytes / 1024 ))                                ## transformar a Kilobytes
 segs=`echo -e "${output}" | grep copied | awk '{print $6}'`   ## obtener segs
