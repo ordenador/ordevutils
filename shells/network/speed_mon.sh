b9b2e3ad0e9bf598d8448e3fde88575b7508e60f
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2015-04-22 18:25:57 -0300

    Create speed_mon.sh

---
+++ shells/network/speed_mon.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+fecha=`date '+%F'`
+hora=`date '+%T'`
+output=$(time dd if=/dev/zero count=10 bs=1024k | ssh nombre-o-ip-servidor dd of=/dev/null 2>&1 > /d                                                                                                   ev/null | awk '$0' ORS='\n')
+bytes=`echo -e "${output}" | grep copied | awk '{print $1}'`  ## obtener bytes
+kilobytes=$(( $bytes / 1024 ))                                ## transformar a Kilobytes
+segs=`echo -e "${output}" | grep copied | awk '{print $6}'`   ## obtener segs
+speed=`echo "${kilobytes} / ${segs}" | bc`                    ## calculo kilobytes/segs
+echo ${fecha} ${hora}";"${speed} >> output.log
